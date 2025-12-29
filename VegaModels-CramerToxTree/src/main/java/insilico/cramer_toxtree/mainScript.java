dab5678f2006f37d7bc7e0e74b8f37c035f27c23
Alberto Manganaro <alberto.manganaro@amcc.it>
2025-12-29 16:41:45 +0100

    updated Cramer module with correct pom

--- VegaModels-CramerToxTree/src/main/java/insilico/cramer_toxtree/mainScript.java
+++ VegaModels-CramerToxTree/src/main/java/insilico/cramer_toxtree/mainScript.java
@@ -6,7 +6,7 @@ import insilico.core.molecule.conversion.SmilesMolecule;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import utils.ModelsDeployment;
+
 
 import java.io.File;
 import java.nio.file.Files;
@@ -31,9 +31,9 @@ public class mainScript {
 
         List<String> smilesList = new ArrayList<>();
         smilesList.add("O=C");
-//        smilesList.add("O=S(=O)(N)c1cc2c(cc1C(F)(F)F)NC(NS2(=O)(=O))Cc3ccccc3");
-//        smilesList.add("O=[N+]([O-])c1cc(c(O)c(c1)C(C)(C)C)[N+](=O)[O-]");
-//        smilesList.add("O=S(=O)(C(C)(C)C)C(C)(C)C");
+        smilesList.add("O=S(=O)(N)c1cc2c(cc1C(F)(F)F)NC(NS2(=O)(=O))Cc3ccccc3");
+        smilesList.add("O=[N+]([O-])c1cc(c(O)c(c1)C(C)(C)C)[N+](=O)[O-]");
+        smilesList.add("O=S(=O)(C(C)(C)C)C(C)(C)C");
 
         for(String smiles : smilesList) {
             InsilicoModelOutput out = model.Execute(SmilesMolecule.Convert(smiles));
